3289558cff0e4f3cc8003f24e4d6645a6e41c7c9
IYODA Atsushi <iyoda-github@atikoro.net>
2010-12-05 02:48:15 +0900

    Raised version.

--- rebloglet.user.js
+++ rebloglet.user.js
@@ -16,7 +16,7 @@
 // @include        http://www.tumblr.com/tagged/*
 // @exclude        http://www.tumblr.com/tumblelog/*/followers
 // @license        Public domain
-// @version        0.3.20100409.0
+// @version        0.3.20101205.0
 // ==/UserScript==
 
 (function(){
